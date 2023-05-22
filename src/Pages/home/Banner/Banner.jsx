0bb3f30b5a52de3ec46e46f5b75234b3be06772b
Avi <avishekuser@gmai.com>
2023-05-22 15:34:38 +0600

    banner & photos gallery done

--- src/Pages/home/Banner/Banner.jsx
+++ src/Pages/home/Banner/Banner.jsx
@@ -9,13 +9,21 @@ import banner4 from '../../../assets/banner4.png'
 import banner5 from '../../../assets/banner5.png'
 import banner6 from '../../../assets/banner6.png'
 import banner7 from '../../../assets/banner7.png'
+import banner8 from '../../../assets/pug.png'
 const Banner = () => {
     return (
 
         <div>
+            <div className='flex'>
+            <div>
+                <h1 className='text-7xl mt-12 '>There is a Animal <br /> Toys <span className='font-semibold text-orange-500'>Collection </span> <br /> You Can Get Any toys <span className='text-pink-500 font-semibold'>from</span> Here</h1>
+            </div>
             <div className='w-2/4'>
                 <Lottie animationData={animation} loop={true}></Lottie>
             </div>
+            </div>
+            <br /><br />
+            <p className='text-4xl mt-8 font-semibold text-center text-violet-800  '>PHotos SCroll GAllery</p>
             <div className="carousel carousel-center rounded-box mt-12 mb-12">
                 <div className="carousel-item w-1/5">
                     <img src={banner1}alt="Pizza" />
@@ -35,8 +43,8 @@ const Banner = () => {
                 <div className="carousel-item w-1/5">
                     <img src={banner7} />
                 </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" />
+                <div className="carousel-item w-1/5">
+                    <img src={banner8} alt="Pizza" />
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {
 import router from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto bg-red-50'>
+  <div className='max-w-7xl mx-auto '>
     <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
